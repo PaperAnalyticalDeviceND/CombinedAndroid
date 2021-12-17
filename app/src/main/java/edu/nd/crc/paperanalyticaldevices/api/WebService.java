f864ce7d9891c78d0c0c7325c0ed636aed983c48
James Sweet <james.sweet88@gmail.com>
2021-12-17 11:13:29 -0500

    feat(api): Initial implementation of the API service and code using the network list call

---
+++ app/src/main/java/edu/nd/crc/paperanalyticaldevices/api/WebService.java
@@ -0,0 +1,42 @@
+package edu.nd.crc.paperanalyticaldevices.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.vdurmont.semver4j.Semver;
+
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface WebService {
+    @GET("/index.php?option=com_jbackend&view=request&module=querytojson&action=get&resource=list&queryname=network_info")
+    Call<ResponseList<NetworkEntry>> GetNetworkInfo(@Query("api_key") String api_key);
+
+    @FormUrlEncoded
+    @POST("/index.php?option=com_jbackend&view=request&module=querytojson&action=post&resource=upload")
+    Call<Response> UploadEntry(@FieldMap Map<String, String> names);
+
+    static WebService instantiate(){
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Semver.class, new NetworkEntry.SemverDeserializer())
+                .registerTypeAdapter(TypeToken.getParameterized(List.class, String.class).getType(), new NetworkEntry.StringListDeserializer())
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://pad.crc.nd.edu/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        return retrofit.create(WebService.class);
+    }
+}
