118c392f3bef74cac16dd032a19359d3cdb1a950
Matthew Curran <mjcurran@ucdavis.edu>
2024-09-11 15:04:05 -0400

    feat:  New neural net API model.

---
+++ app/src/main/java/edu/nd/crc/paperanalyticaldevices/api/NetworkV2.java
@@ -0,0 +1,71 @@
+package edu.nd.crc.paperanalyticaldevices.api;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import com.google.gson.annotations.SerializedName;
+import com.vdurmont.semver4j.Semver;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class NetworkV2 {
+
+    @SerializedName("id")
+    public int Id;
+
+    @SerializedName("name")
+    public String Name;
+
+    @SerializedName("drugs_size")
+    public int DrugsSize;
+
+    @SerializedName("drugs")
+    public List<String> Drugs;
+
+    @SerializedName("labels")
+    public List<String> Labels;
+
+    @SerializedName("lanes_excluded")
+    public String LanesExcluded;
+
+    @SerializedName("weights_url")
+    public String Weights;
+
+    @SerializedName("image_size")
+    public String ImageSize;
+
+    @SerializedName("brightness")
+    public Float Brightness;
+
+    @SerializedName("type")
+    public String Type;
+
+    @SerializedName("description")
+    public String Description;
+
+    @SerializedName("test")
+    public String Test;
+
+    @SerializedName("version")
+    public Semver Version;
+
+    @SerializedName("SHA256")
+    public String Hash;
+
+    public static class StringListDeserializer implements JsonDeserializer<List<String>> {
+        @Override
+        public List<String> deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            return Arrays.stream(json.getAsJsonPrimitive().getAsString().split(",")).collect(Collectors.toList());
+        }
+    }
+
+    public static class SemverDeserializer implements JsonDeserializer<Semver> {
+        @Override
+        public Semver deserialize(JsonElement json, java.lang.reflect.Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            return new Semver(json.getAsJsonPrimitive().getAsString(), Semver.SemverType.LOOSE);
+        }
+    }
+}
